d64d8fb5350e1038e44eb46d6f408209294368d5
aurora <auroras.medias@gmail.com>
2024-03-09 18:11:08 -0800

    began work on color palette display/editor

---
+++ addons/picky-pixels/color_palette.gd
@@ -0,0 +1,30 @@
+@tool
+extends Control
+
+const COLOR_PICKER = preload("res://addons/picky-pixels/tabs/color_picker.tscn")
+@onready var color_picker_container = $ScrollContainer/ColorPickerContainer
+
+
+var _colors: Array[Color] = []
+@export var colors: Array[Color] = []:
+	get: return _colors
+	set(val):
+		_colors = val
+		if color_picker_container == null:
+			await ready
+		# Add/update colors to color_picker_container as needed
+		for i in _colors.size():
+			var color = _colors[i]
+			# If there is an existing color picker, update the color
+			if i < color_picker_container.get_child_count():
+				color_picker_container.get_child(i).color = color
+			# If there is not, create a new color picker
+			else:
+				var color_picker = COLOR_PICKER.instantiate()
+				color_picker.label = str(i)
+				color_picker.color = color
+				color_picker.color_locked = true
+				color_picker_container.add_child(color_picker)
+		# Remove any extra color picker nodes, if needed
+		for i in range(_colors.size(), color_picker_container.get_child_count()):
+			color_picker_container.remove_child(color_picker_container.get_child(i))
--- addons/picky-pixels/tabs/color_picker.gd
+++ addons/picky-pixels/tabs/color_picker.gd
@@ -1,11 +1,13 @@
 @tool
 extends Control
 
+signal color_changed(c: Color)
+
 @onready var _label_node: Label = $Label
 @onready var _color_picker_button = $ColorPickerButton
 
-var _label: String = "Color 1"
-@export var label: String = "Color 1":
+var _label: String = "0"
+@export var label: String = "0":
 	get: return _label
 	set(val):
 		_label = val
@@ -17,10 +19,23 @@ var _color: Color
 @export var color: Color:
 	get: return _color
 	set(val):
+		if _color == val:
+			return
 		_color = val
 		if _color_picker_button == null:
 			await ready
 		_color_picker_button.color = val
+		color_changed.emit(val)
+
+var _color_locked: bool
+## Whether the color can be edited by the user.
+@export var color_locked: bool = false:
+	get: return _color_locked
+	set(val):
+		_color_locked = val
+		if _color_picker_button == null:
+			await ready
+		_color_picker_button.disabled = true
 
 func _on_color_picker_button_color_changed(c):
 	_color = c
