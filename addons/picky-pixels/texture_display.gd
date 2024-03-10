37388ca9a6482de87adbeff16c7339ed17a0d620
aurora <auroras.medias@gmail.com>
2024-03-09 17:18:40 -0800

    texture display now keeps texture in middle; light level tabs are now functional: tabs switch between textures based on which one is enabled

--- addons/picky-pixels/texture_display.gd
+++ addons/picky-pixels/texture_display.gd
@@ -1,23 +1,77 @@
 @tool
-extends TextureRect
+extends Panel
 
+@onready var texture_rect = $TextureRect
 @onready var label = $Label
 @onready var remove_button = $RemoveButton
+@onready var zoom_in_button = $ZoomInButton
+@onready var zoom_out_button = $ZoomOutButton
 var png_regex: RegEx
+var image_size: Vector2i
+var zoom_percent: float = 1.0
+
+
+func get_texture() -> Texture2D:
+	return texture_rect.texture
+
+
+func set_texture(texture):
+	if texture != null:
+		_set_non_null_texture(texture)
+	else:
+		_reset()
+
+
+func _set_non_null_texture(texture: Texture2D):
+	# Adjust enabled/visible nodes
+	label.visible = false
+	remove_button.visible = true
+	remove_button.disabled = false
+	zoom_in_button.visible = true
+	zoom_in_button.disabled = false
+	zoom_out_button.visible = true
+	zoom_out_button.disabled = false
+	
+	# Update data to display
+	texture_rect.texture = texture
+	image_size = Vector2i(texture.get_size())
+	tooltip_text = texture.resource_path
+	
+	# Center the image and zoom based on the new image
+	_adjust_zoom()
+
 
 func _reset():
 	label.visible = true
 	remove_button.visible = false
 	remove_button.disabled = true
+	zoom_in_button.visible = false
+	zoom_in_button.disabled = true
+	zoom_out_button.visible = false
+	zoom_out_button.disabled = true
 	tooltip_text = ""
-	texture = null
+	texture_rect.texture = null
+
 
 func _ready():
 	texture_filter = CanvasItem.TEXTURE_FILTER_NEAREST
+	# Set icons
 	remove_button.icon = get_theme_icon("Remove", "EditorIcons")
+	zoom_in_button.icon = get_theme_icon("ZoomMore", "EditorIcons")
+	zoom_out_button.icon = get_theme_icon("ZoomLess", "EditorIcons")
 	png_regex = RegEx.new()
 	png_regex.compile(".*\\.png$")
 	_reset()
+
+
+func _adjust_zoom():
+	var new_size = image_size * zoom_percent
+	var half_new_size = Vector2i(new_size / 2.0)
+	texture_rect.offset_left = -1 * half_new_size.x
+	texture_rect.offset_top = -1 * half_new_size.y
+	texture_rect.offset_right = half_new_size.x
+	texture_rect.offset_bottom = half_new_size.y
+
 
 func _can_drop_data(at_position, data):
 	return (
@@ -28,13 +82,20 @@ func _can_drop_data(at_position, data):
 		png_regex.search(data.files[0]) != null
 	)
 
+
 func _drop_data(at_position, data):
-	label.visible = false
-	remove_button.visible = true
-	remove_button.disabled = false
-	var image_path = data.files[0]
-	tooltip_text = image_path
-	texture = load(image_path)
+	_set_non_null_texture(load(data.files[0]))
+
 
 func _on_remove_button_pressed():
 	_reset()
+
+
+func _on_zoom_out_button_pressed():
+	zoom_percent /= 1.25
+	_adjust_zoom()
+
+
+func _on_zoom_in_button_pressed():
+	zoom_percent *= 1.25
+	_adjust_zoom()
--- addons/picky-pixels/texture_editor.gd
+++ addons/picky-pixels/texture_editor.gd
@@ -3,19 +3,62 @@ extends VBoxContainer
 
 const LIGHT_LEVEL_TAB = preload("res://addons/picky-pixels/light_level_tab.tscn")
 @onready var light_levels_tabs = $Main/LightLevelData/ScrollContainer/LightLevelsTabs
+@onready var light_levels_spin_box = $Main/Config/LightLevelsConfig/SpinBox
+@onready var texture_display = $Main/LightLevelData/TextureDisplay
+var selected_tab: int
+# Index is the light level tab
+var textures: Array[Texture2D] = []
 
 
-func _on_light_level_value_changed(value):
+func _on_light_level_tab_button_pressed(index: int):
+	# Unselect previous tab
+	if selected_tab != index:
+		# Update stored texture
+		textures[selected_tab] = texture_display.get_texture()
+		
+		# Set pressed with no signal since the signal is listened to in
+		# _on_light_level_tab_toggled
+		light_levels_tabs.get_child(selected_tab).set_pressed_no_signal(false)
+		selected_tab = index
+		
+		# Update displayed texture
+		texture_display.set_texture(textures[selected_tab])
+
+
+func _on_light_level_tab_toggled(index: int):
+	# Prevent unselecting current tab
+	if selected_tab == index:
+		light_levels_tabs.get_child(selected_tab).button_pressed = true
+
+
+func _on_light_levels_value_changed(value):
 	var value_diff = value - light_levels_tabs.get_child_count()
 	
 	if value_diff > 0:
 		for i in value_diff:
 			var new_tab = LIGHT_LEVEL_TAB.instantiate()
-			new_tab.text = str(light_levels_tabs.get_child_count() + 1)
+			var index = light_levels_tabs.get_child_count()
+			new_tab.text = str(index)
+			new_tab.tooltip_text = "View light level %s texture" % str(index)
+			new_tab.button_down.connect(
+				func(): _on_light_level_tab_button_pressed(index)
+			)
+			new_tab.toggled.connect(
+				func(toggled_on): _on_light_level_tab_toggled(index)
+			)
 			light_levels_tabs.add_child(new_tab)
+			# Update array so it is the correct length
+			textures.append(null)
 	elif value_diff < 0:
 		light_levels_tabs.get_children()
 		for i in abs(value_diff):
 			light_levels_tabs.remove_child(
 				light_levels_tabs.get_children().back()
 			)
+			# Update array so it is the correct length
+			textures.pop_back()
+		# Update selected light level tab if needed
+		if selected_tab >= light_levels_tabs.get_child_count():
+			selected_tab = light_levels_tabs.get_child_count() - 1
+			light_levels_tabs.get_children().back().set_pressed_no_signal(true)
+			texture_display.set_texture(textures[selected_tab])
