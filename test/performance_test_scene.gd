374368ae977040c2936c2987f50a679811d5f13c
aurora <auroras.medias@gmail.com>
2024-04-16 17:10:18 -0700

    updated performance test and demo scene: light gradients go from white to black, updated how perf test scene places nodes

--- test/performance_test_scene.gd
+++ test/performance_test_scene.gd
@@ -1,29 +1,43 @@
 extends Node2D
 
-#const TEST_SPRITE = preload("res://test/control_sprite.tscn")
-const TEST_SPRITE = preload("res://addons/picky-pixels/nodes/picky_sprite_2d/picky_sprite_2d.tscn")
+#const SPRITE = preload("res://test/control_sprite.tscn")
+const SPRITE = preload("res://addons/picky-pixels/nodes/picky_sprite_2d/picky_sprite_2d.tscn")
 const TEST_LIGHT = preload("res://test/test_light.tscn")
 
-@export var sprites: int = 50
+@export var rows: int = 10
+@export var cols: int = 10
 @export var lights: int = 5
+@export var sprite_size: Vector2i = Vector2i(32, 32)
 
 # GOAL:
 # Get this scene to run at a consistent 60 FPS with 300 sprites and 32 lights
 
 func _ready():
-	var x_dim = max(1, floor(sqrt(sprites * 4.0/3.0)))
-	var y_dim = round(x_dim * 3.0/4.0)
-	randomize()
-	for x in x_dim:
-		for y in y_dim:
-			var pos = 32.0 * Vector2(x, y) + Vector2(16, 16)
-			var sprite = TEST_SPRITE.instantiate()
-			sprite.position = pos
+	for x in cols:
+		for y in rows:
+			var sprite = SPRITE.instantiate()
+			sprite.position = Vector2(x * sprite_size.x, y * sprite_size.y) + (sprite_size / 2.0)
 			add_child(sprite)
-	
+	 
 	for _i in lights:
-		var pos = Vector2(randf_range(0.0, 32.0 * x_dim), randf_range(0.0, 32.0 * y_dim)) + Vector2(16, 16)
-		var light = TEST_LIGHT.instantiate()
-		light.position = pos
-		add_child(light)
+		var light: PointLight2D = TEST_LIGHT.instantiate()
+		light.position = Vector2(
+			randf_range(32.0, 32.0 * (cols-1)),
+			randf_range(32.0, 32.0 * (rows-1))
+		)
 		light.play_random()
+		add_child(light)
+
+
+func _process(delta):
+	var move_dir = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		move_dir.y += 1
+	if Input.is_action_pressed("ui_down"):
+		move_dir.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		move_dir.x += 1
+	if Input.is_action_pressed("ui_right"):
+		move_dir.x -= 1
+	
+	position += Vector2(delta * 500.0 * move_dir).round()
